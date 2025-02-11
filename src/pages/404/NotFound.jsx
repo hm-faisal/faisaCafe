f30c20f0fa0960c49f70750cd5720d2499be03ef
hm-faisal <contact.hmfaisal@gmail.com>
2025-02-11 19:49:13 +0600

    404 page updated

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,18 +1,30 @@
 import { Link } from "react-router";
+import useDevice from "../../hooks/useDevice";
+import { Helmet } from "react-helmet";
 
 const NotFound = () => {
+  const { darkTheme, minHeight, siteName } = useDevice();
   return (
     <>
-      <div className="flex justify-center items-center min-h-screen flex-col gap-8">
-        <h2>
-          <span className="text-9xl font-bold text-accent">4</span>
-          <span className="text-9xl font-bold text-secondary">0</span>
-          <span className="text-9xl font-bold text-accent">4</span>
-        </h2>
-        <p className="text-2xl font-bold ">Oh ! Page Not Found !!!</p>
-        <Link to={"/"} className="btn btn-accent">
-          Back to Home
-        </Link>
+      <div data-theme={darkTheme ? "dark" : "light"}>
+        <Helmet>
+          <title>Home || {siteName}</title>
+        </Helmet>
+        <div
+          className={`flex justify-center items-center flex-col min-h-screen`}
+        >
+          <div className="space-y-2 text-center">
+            <h2>
+              <span className="text-9xl font-bold text-yellow-700">4</span>
+              <span className="text-9xl font-bold text-secondary">0</span>
+              <span className="text-9xl font-bold text-yellow-700">4</span>
+            </h2>
+            <p className="text-2xl font-bold ">Oh ! Page Not Found !!!</p>
+            <Link to={"/"} className="btn btn-accent">
+              Back to Home
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
